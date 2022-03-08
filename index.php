6d91e5376ce3b6db5103cc34ccaf12811635c25e
Tatiana Sanchez <tatiana.sanchez.sanin@gmail.com>
2022-03-08 16:52:41 -0500

    Cambios

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-    session_start();
+session_start();
 
-    if (isset($_SESSION['usuario'])) {
-        header("location: bienvenida.php");
-    }
+if (isset($_SESSION['usuario'])) {
+    header("location: bienvenida.php");
+}
 ?>
 
 
@@ -21,45 +21,23 @@
 </head>
 
 <body>
-    <main>
-        <div class="contenedor_general">
-            <div class="caja_fondo">
-                <div class="caja_fondo-login">
-                    <h3>¿Ya tienes una cuenta?</h3>
-                    <p>Inicia Sesion para entrar al sistema</p>
-                    <button id="btn_iniciar-sesion">Iniciar Sesion</button>
-                </div>
-                <div class="caja_fondo-registro">
-                    <h3>¿Aún no tienes una cuenta?</h3>
-                    <p>Registrate para iniciar sesion</p>
-                    <button id="btn_registrar">Registarse</button>
-                </div>
-            </div>
 
-            <!-- Formulario de login y registro -->
-            <div class="contenedor_login-registro">
-                <!-- Login -->
-                <form action = "PHP/inicioSesionUsuario_BackEnd.php" method = "POST" class="formulario_login">
-                    <h2>Iniciar Sesion</h2>
-                    <input type="text" placeholder="Usuario" name="usuario_inicioSesion">
-                    <input type="password" placeholder="Contraseña" name="contrasena_inicioSesion">
-                    <button>Entrar</button>
-                </form>
+    <div class="contenedor_formulario">
+        <i class="fa-solid fa-user logo"></i>
+        <!-- Login -->
+        <h2>Iniciar Sesion</h2>
+        <form action="PHP/inicioSesionUsuario_BackEnd.php" method="POST" class="formulario_login">
+            <label for="username">Usuario</label>
+            <input type="text" placeholder="Usuario" name="usuario_inicioSesion">
 
-                <!-- Registro -->
-                <form action = "PHP/registroUsuario_BackEnd.php" method="POST" class="formulario_registro">
-                    <h2>Registrarse</h2>
-                    <input type="text" placeholder="Nombre completo" name="nombre_completo">
-                    <input type="text" placeholder="Correo Electrónico" name="correo">
-                    <input type="text" placeholder="Usuario" name="usuario">
-                    <input type="password" placeholder="Contraseña" name="contrasena">
-                    <button>Registrarse</button>
-                </form>
-            </div>
-        </div>
-    </main>
+            <label for="password">Contraseña</label>
+            <input type="password" placeholder="Contraseña" name="contrasena_inicioSesion">
 
-    <script src="JS/script.js"></script>
+            <button>Entrar</button>
+
+            <a href="#">Has perdido tu contraseña?</a>
+        </form>
+    </div>
 </body>
 
 </html>
